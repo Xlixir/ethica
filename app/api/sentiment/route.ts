52c5c0bcbf0511ef379ab9bf9620b9d839b704d8
Hemang <hemang.19je0365@ap.iitism.ac.in>
2023-09-14 22:13:51 +0530

    Final commit

--- app/api/sentiment/route.ts
+++ app/api/sentiment/route.ts
@@ -65,57 +65,142 @@
 // }
 
 
+ 
+// pages/api/analyzeReview.ts
+// import type { NextApiRequest, NextApiResponse } from 'next';
+// import cohere from 'cohere-ai';
+
+// cohere.init(process.env.COHERE_API_KEY);
+
+// const examples = [
+  
+//   { text: 'The order came 5 days early', label: 'positive review' },
+//   { text: 'The item exceeded my expectations', label: 'positive review' },
+//   { text: 'I ordered more for my friends', label: 'positive review' },
+//   { text: 'I would buy this again', label: 'positive review' },
+//   { text: 'I would recommend this to others', label: 'positive review' },
+//   { text: 'The package was damaged', label: 'negative review' },
+//   { text: 'The order is 5 days late', label: 'negative review' },
+//   { text: 'The order was incorrect', label: 'negative review' },
+//   { text: 'I want to return my item', label: 'negative review' },
+//   { text: 'The items material feels low quality', label: 'negative review' },
+//   { text: 'The product was okay', label: 'neutral review' },
+//   { text: 'I received five items in total', label: 'neutral review' },
+//   { text: 'I bought it from the website', label: 'neutral review' },
+//   { text: 'I used the product this morning', label: 'neutral review' },
+//   { text: 'The product arrived yesterday', label: 'neutral review' }
+// ];
+
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+//   if (req.method === 'POST') {
+//     const { reviewText } = req.body;
+
+//     const response = await cohere.classify({
+//       inputs: [reviewText],
+//       examples: examples,
+//     });
+
+//     const classification = response.body.classifications[0].prediction;
+
+//     res.status(200).json({ label: classification });
+//   } else {
+//     res.status(405).end(); // Method Not Allowed
+//   }
+// };
+
+
+// import type { NextApiRequest, NextApiResponse } from 'next';
+// import cohere from 'cohere-ai';
+
+// cohere.init(process.env.COHERE_API_KEY);
+
+// const examples = [
+  
+//   { text: 'The order came 5 days early', label: 'positive review' },
+//   { text: 'The item exceeded my expectations', label: 'positive review' },
+//   { text: 'I ordered more for my friends', label: 'positive review' },
+//   { text: 'I would buy this again', label: 'positive review' },
+//   { text: 'I would recommend this to others', label: 'positive review' },
+//   { text: 'The package was damaged', label: 'negative review' },
+//   { text: 'The order is 5 days late', label: 'negative review' },
+//   { text: 'The order was incorrect', label: 'negative review' },
+//   { text: 'I want to return my item', label: 'negative review' },
+//   { text: 'The items material feels low quality', label: 'negative review' },
+//   { text: 'The product was okay', label: 'neutral review' },
+//   { text: 'I received five items in total', label: 'neutral review' },
+//   { text: 'I bought it from the website', label: 'neutral review' },
+//   { text: 'I used the product this morning', label: 'neutral review' },
+//   { text: 'The product arrived yesterday', label: 'neutral review' }
+// ];
+
+// export async function POST(req: NextApiRequest, res: NextApiResponse) {
+//   const { reviewText } = req.body;
+//   if (!reviewText || reviewText.trim() === "") {
+//     return res.status(400).json({ error: 'Review text cannot be empty' });
+// }
+
+
+// const response = await cohere.classify({
+//   inputs: [reviewText],
+//   examples: examples,
+// });
+
+//  console.log(response)
+
+//   const classification = response.body.classifications[0].prediction;
+
+//   res.status(200).json({ label: classification });
+// }
+
+
 import type { NextApiRequest, NextApiResponse } from 'next';
 import cohere from 'cohere-ai';
 
-cohere.init(process.env.COHERE_API_KEY);
+cohere.init("YMa7BqCcmg0Dtd8txRpuKMelVyOkkz7Q8wQvHelL");
 
-  const POSITIVE_SAMPLES = [
-    "The reception was great. I love how your staff welcomed us.",
-    "Your staff took us on a tour around the hotel, and I really enjoyed that.",
-    "The bedroom we stayed in was very peaceful. Also loved the paintings on the walls.",
-    "The amenities in the room were great and the rooms were also clean.",
-    "I have stayed in many hotels but this one tops it for me. Enjoyed the customer service and the clean rooms.",
-  ];
+const examples = [
+  { text: 'The order came 5 days early', label: 'positive review' },
+  { text: 'The item exceeded my expectations', label: 'positive review' },
+  { text: 'I ordered more for my friends', label: 'positive review' },
+  { text: 'I would buy this again', label: 'positive review' },
+  { text: 'I would recommend this to others', label: 'positive review' },
+  { text: 'The package was damaged', label: 'negative review' },
+  { text: 'The order is 5 days late', label: 'negative review' },
+  { text: 'The order was incorrect', label: 'negative review' },
+  { text: 'I want to return my item', label: 'negative review' },
+  { text: 'The items material feels low quality', label: 'negative review' },
+  { text: 'The product was okay', label: 'neutral review' },
+  { text: 'I received five items in total', label: 'neutral review' },
+  { text: 'I bought it from the website', label: 'neutral review' },
+  { text: 'I used the product this morning', label: 'neutral review' },
+  { text: 'The product arrived yesterday', label: 'neutral review' }
+];
 
-  const NEGATIVE_SAMPLES = [
-    "The room was not neat or clean. There was no air conditioning system.",
-    "The Wi-Fi in the hotel was poor. I couldn't use the internet while I was there.",
-    "The lady who showed us our room had a very rude attitude.",
-    "Poor customer service and the rooms had a weird smell.",
-    "The air conditioning system was faulty, so the room was very hot.",
-  ];
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  
+  const { reviewText } = req.body;
+  console.log(req.body)
+  if (!reviewText || reviewText.trim() === "") {
+    return res.status(400).json({ error: 'Review text cannot be empty' });
+  }
 
-async function classifyReview(input: string) {
-  const response = await cohere.classify({
-    model: "medium",
-    taskDescription: "",
-    outputIndicator: "",
-    inputs: [input],
-    examples: POSITIVE_SAMPLES.map((sample) => ({
-      text: sample,
-      label: "positive",
-    })).concat(
-      NEGATIVE_SAMPLES.map((sample) => ({ text: sample, label: "negative" }))
-    ),
-  });
+  try {
+    const response = await cohere.classify({
+       inputs: [reviewText],
+       examples: examples,
+    });
+ 
+    const classification = response.body.classifications[0].prediction;
+    res.status(200).json({ label: classification });
+ } catch (error) {
+    console.error("Error when classifying:", error);
+    res.status(500).json({ error: 'Internal server error' });
+ }
+ 
+  const classification = response.body.classifications[0].prediction;
 
-  return response.body.classifications[0];
+  res.status(200).json({ label: classification });
 }
-
-const POST = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { name, number, review } = req.body;
-
-  const classificationResult = await classifyReview(review);
-
-  const label = classificationResult.prediction;
-
-  // Here you can do whatever you want with the data (like storing it)
-  const data = { name, number, review, label };
-
-  res.status(200).json(data);
-};
-
 
 
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,161 +1,110 @@
-"use client";
+"use client"
 
- 
-// import axios from "axios";
-// import { UserButton, useUser } from "@clerk/nextjs";
-// import { useEffect, useState } from 'react';
-// import { useAuth } from "@clerk/nextjs";
-// import { ModeToggle } from "./components/toggle"; 
+import axios from "axios";
+import { UserButton, useUser } from "@clerk/nextjs";
+import { useEffect, useState } from 'react';
+import { ModeToggle } from "./components/toggle"; 
 
-// type Review = {
-//   text: string;
-//   upvotes: number;
-//   downvotes: number;
-//   sentiment?: 'positive' | 'negative' | 'neutral' | 'unknown';
-//   date: Date;
-// }
+type Review = {
+  text: string;
+  upvotes: number;
+  downvotes: number;
+  classification?: 'positive' | 'negative' | 'neutral' | 'unknown';
+  date: Date;
+}
 
-// const Home: React.FC = () => {
-//   const { isLoaded, isSignedIn, user } = useUser();
-//   const [review, setReview] = useState<string>("");
-//   const [reviews, setReviews] = useState<Review[]>([]);
+const Home: React.FC = () => {
+  const { isLoaded, isSignedIn, user } = useUser();
+  const [review, setReview] = useState<string>("");
+  const [reviews, setReviews] = useState<Review[]>([]);
 
-//   useEffect(() => {
-//     if (isLoaded && !isSignedIn) {
-//       window.location.href = process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL as string;
-//     }
-//   }, [isLoaded, isSignedIn]);
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      window.location.href = process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL as string;
+    }
+  }, [isLoaded, isSignedIn]);
 
-//   const handleAddReview = async () => {
-//     const sentiment = await handleAnalyse();
-//     setReviews([...reviews, { text: review, upvotes: 0, downvotes: 0, sentiment, date: new Date() }]);
-//     setReview("");
-//   };
+  const handleAddReview = async () => {
+    const classification = await handleAnalyse();
+    const newReviews = [...reviews, { text: review, upvotes: 0, downvotes: 0, classification, date: new Date() }];
+    setReviews(sortReviewsByVotes(newReviews));
+    setReview("");
+  };
 
-//   const handleAnalyse = async (): Promise<'positive' | 'negative' | 'neutral' | 'unknown'> => {
-//     try {
-//       const res = await axios.post('/api/sentiment', { reviews: review });
+  const sortReviewsByVotes = (reviews: Review[]) => {
+    return reviews.sort((a, b) => (b.upvotes - b.downvotes) - (a.upvotes - a.downvotes));
+  };
 
-//       const data = await res.json();
-//       return data.sentiment;
-//     } catch (error) {
-//       console.error("Error analyzing sentiment:", error);
-//       return user.firstName || "unknown";
-//     }
-//   };
+  const handleAnalyse = async (): Promise<'positive' | 'negative' | 'neutral' | 'unknown'> => {
+    try {
+      const res = await fetch('/api/sentiment', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ reviewText: review })
+      });
 
-//   const handleUpvote = (index: number) => {
-//     const newReviews = [...reviews];
-//     newReviews[index].upvotes += 1;
-//     setReviews(newReviews);
-//   };
-
-//   const handleDownvote = (index: number) => {
-//     const newReviews = [...reviews];
-//     newReviews[index].downvotes -= 1;
-//     setReviews(newReviews);
-//   };
-
-//   const sortedReviews = reviews.sort((a, b) => (b.upvotes - b.downvotes) - (a.upvotes - a.downvotes));
-
-//   return (
-//     <div className="flex flex-col justify-between items-start w-full h-screen p-4">
-//       <div className="flex justify-between w-full">
-//         <h1 className="text-4xl font-bold text-black-600">ethica</h1>
-//         <div className="flex space-x-4">
-//           <ModeToggle />
-//           <UserButton afterSignOutUrl="/"/>
-//         </div>
-//       </div>
-
-//       <div className="flex flex-col space-y-4 w-full mt-8">
-//         <textarea 
-//           className="resize-none border rounded p-2 w-full" 
-//           rows={5} 
-//           placeholder="Enter your review" 
-//           value={review} 
-//           onChange={(e) => setReview(e.target.value)} 
-//         />
-//         <div className="flex space-x-4">
-//           <button className="bg-blue-500 text-white rounded p-2" onClick={handleAddReview}>Add Review</button>
-//           <button className="bg-green-500 text-white rounded p-2" onClick={handleAnalyse}>Analyse</button>
-//         </div>
-//       </div>
-
-//       <div className="mt-8">
-//         {sortedReviews.map((r, index) => (
-//           <div key={index} className="flex justify-between border-b pb-2 mb-2">
-//             <div>
-//               <p>{r.text} <span className="text-gray-500">({r.sentiment})</span></p>
-//               <p className="text-sm text-gray-400">{r.date.toLocaleString()}</p>
-//             </div>
-//             <div>
-//               <button onClick={() => handleUpvote(index)}>👍 {r.upvotes}</button>
-//               <button onClick={() => handleDownvote(index)}>👎 {r.downvotes}</button>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Home;
-
-import { useState } from 'react';
-
-const Home = () => {
-  const [reviewText, setReviewText] = useState('');
-  const [classification, setClassification] = useState<string | null>(null);
-
-  async function handleSubmit() {
-    const response = await fetch('/api/sentiment', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ name: "Example Name", number: "12345", review: reviewText })
-    });
-
-    const data = await response.json();
-    setClassification(data.label);
-  }
+      const data = await res.json();
+      return data.label;
+    } catch (error) {
+      return user.firstName || 'unknown';
+    }
+  };
 
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Review Classifier</h1>
-
-      <textarea 
-        rows={5} 
-        cols={50} 
-        value={reviewText} 
-        onChange={(e) => setReviewText(e.target.value)}
-        placeholder="Enter your review here..."
-      ></textarea>
-
-      <button onClick={handleSubmit} style={{ display: "block", marginTop: "10px" }}>
-        Submit Review
-      </button>
-
-      {classification && (
-        <div style={{ marginTop: "20px" }}>
-          <strong>Review Classification:</strong> {classification}
+    <div className="flex flex-col justify-between items-start w-full h-screen p-4">
+      <div className="flex justify-between w-full">
+        <h1 className="text-4xl font-bold italic text-black-600 hover:text-blue-700 transition duration-300 transform hover:scale-105 hover:cursor-pointer">
+          ethica
+        </h1>
+        <div className="flex space-x-4">
+          <ModeToggle />
+          <UserButton afterSignOutUrl="/"/>
         </div>
-      )}
+      </div>
+
+      <div className="flex flex-col space-y-4 w-full mt-8">
+        <textarea 
+           className="resize-none border rounded p-2 w-full focus:ring focus:ring-blue-200 transition duration-300" 
+          rows={5} 
+          placeholder="Enter your review" 
+          value={review} 
+          onChange={(e) => setReview(e.target.value)} 
+        />
+        <div className="flex space-x-4">
+          <button className="bg-blue-500 hover:bg-blue-600 text-white rounded p-2 transition duration-300" onClick={handleAddReview}>Add Review</button>
+          <button className="bg-green-500 hover:bg-green-600 text-white rounded p-2 transition duration-300" onClick={handleAnalyse}>Analyse</button>
+        </div>
+      </div>
+
+      <div className="mt-8">
+        {reviews.map((r, index) => (
+          <div key={index} className="flex justify-between border-b pb-2 mb-2">
+            <div>
+              <p>{r.text} <span className="text-gray-500">({r.classification})</span></p>
+              <p className="text-sm text-gray-400">{r.date.toLocaleString()}</p>
+            </div>
+            <div>
+              <button onClick={() => {
+                const updatedReviews = [...reviews];
+                updatedReviews[index].upvotes++;
+                setReviews(sortReviewsByVotes(updatedReviews));
+              }}>👍 {r.upvotes}</button>
+              <button onClick={() => {
+                const updatedReviews = [...reviews];
+                updatedReviews[index].downvotes--;
+                setReviews(sortReviewsByVotes(updatedReviews));
+              }}>👎 {r.downvotes}</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default Home;
-
-
-
-
- 
-
-
-
-
 
 
  
